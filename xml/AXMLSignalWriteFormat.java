7e1d5a3d4533f0b197f5c567f754409f6dfb5042
sztejkat <sztejkat@yahoo.com>
2021-12-04 23:50:03 +0100

    Written code checking if signal can be used as XML element name, needs to test it. Not compiling

--- xml/AXMLSignalWriteFormat.java
+++ xml/AXMLSignalWriteFormat.java
@@ -43,7 +43,22 @@ public abstract class AXMLSignalWriteFormat extends ASignalWriteFormat
 			this.ESCAPE_CHAR=ESCAPE_CHAR;
 			this.ESCAPE_CHAR_END=ESCAPE_CHAR_END;
 		};
+		/* ******************************************************************
 		
+				Services required from subclasses
+				
+		
+		* ******************************************************************/
+		/** Should check if specified signal_name can be used as an XML element name
+		or do it clash with XML elements used by this writer.
+		<p>
+		Used to test if use short or long begin signal form.
+		@param element a name of XML element to check, fitting XML rules, non null.
+		@return true if it clashes and cannot be used in short form.
+				Subclasses may enforce long form by always returning false from this 
+				method. Enforcing of short form is NOT possible.
+		*/
+		protected abstract boolean isReservedElement(String signal_name);
 		/* ******************************************************************
 		
 				Services tunable by subclasses
@@ -79,6 +94,86 @@ public abstract class AXMLSignalWriteFormat extends ASignalWriteFormat
 			};
 			a.append(ESCAPE_CHAR_END);
 		};
+		/** Tests if specified character is a valid character which can be put into
+		the XML stream inside a text representing <code>char[]</code> block.
+		<p>
+		Default implementation tests againts rules 
+		specified in <a href="doc-files/xml-syntax.html#escape_in_char">xml syntax definition</a>.
+		<p>
+		Subclasses should also return false if character cannot be encoded with a low
+		level stream char-set (ie, ISO-xxx 8 bit code page).
+		@param c character
+		@return true if character does not need encoding,
+				false if it needs to be passed through {@link #escape}
+		*/
+		protected boolean isValidTextChar(char c)
+		{
+			//first check if it is an escape char
+			if (c==ESCAPE_CHAR) return false;
+			if (
+					((c>=0x20)&&(c<=0xD7FF))
+						||
+					((c>=0xE000)&&(c<=0xFFFD))
+				)
+			{
+					//this is allowed XML set, but now we need to remove non-recommended.
+					if (
+							((c>=0x7F)&&(c<=0x84))
+								||
+							((c>=0x86)&&(c<=0x9F))
+								||
+							((c>=0xFDD0)&&(c<=0xFDDF))
+								||
+							((c=='>')||(c=='<')||(c=='&')) //xml elements and entities starts
+								||
+							(Character.isWhiteSpace(c))
+						)	return false;
+					return true;
+			}else
+				return false;
+		};
+		/** Tests if specified character is a valid character which can be put into
+		the XML stream inside an attribute value
+		<p>
+		Default implementation returns true if {@link #isValidTextChar} and not "'
+		@param c character to validate
+		@return  true if character does not need encoding,
+				false if it needs to be passed through {@link #escape}
+		*/
+		protected boolean isValidAttributeChar(char c)
+		{
+			if (isValidTextChar(c)) return true;
+			return !((c=='\'')||(c=='\"'));
+		};
+		/** Tests if specified signal name (as passed to {@link #begin} )
+		can be used directly as XML element name
+		<a href="doc-files/xml-syntax.html#short_signal_form">in a short form</a>
+		or if it must be encoded using 
+		<a href="doc-files/xml-syntax.html#long_signal_form">in a long form</a>.
+		<p>
+		Default implementation tests if XML rules are met and asks {@link #isReservedXMLElement}
+		if there is no name clash.
+		@param signal_name name to check, non null.
+		@return true if can use short form
+		@see #isReservedXMLElement
+		*/
+		protected boolean isPossibleXMLElement(String signal_name)
+		{
+			assert(signal_name!=null);
+			final int L = signal_name.length();
+			if (L==0) return false; //empty element name is not allowed
+			//first char must be a letter.
+			char c = signal_name.charAt(0);
+			if (!Character.isLetter(c)) return false;
+			//all characters must be letters or digits.
+			for(int i = L; --i>=1; 0)
+			{
+				char c = signal_name.charAt(i);
+				if (!(Character.isLetter(c) || Character.isDigit(c) || (c=='_') || (c=='.'))) return false;
+			};
+			return !isReservedElement(signal_name);
+		};
+		
 		/* *****************************************************************************
 		
 		
