7378c700a98b03f92d9038a42064532652ec763c
sztejkat <sztejkat@yahoo.com>
2021-12-04 22:02:51 +0100

    Beginning work on XML format

---
+++ xml/CXMLSignalWriteFormat.java
@@ -0,0 +1,16 @@
+package sztejkat.abstractfmt.xml;
+import sztejkat.abstractfmt.*;
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+	A writer which is writing data to a stream using XML
+	like format.
+	<p>
+	For used XML format definition see <a href="doc-files/xml-syntax.html">there</a>.
+*/
+public abstract class CXMLSignalWriteFormat //extends ASignalWriteFormat
+{
+	
+		
+};
