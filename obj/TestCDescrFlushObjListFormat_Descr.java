3796578c0775422942b311400043e8ea50c59ab1
- <->
2021-12-11 15:15:33 +0100

    Added described test for streams with flush indicators.

---
+++ obj/TestCDescrFlushObjListFormat_Descr.java
@@ -0,0 +1,29 @@
+package sztejkat.abstractfmt.obj;
+import sztejkat.abstractfmt.*;
+/**
+	An interoperational tests
+	for {@link ASignalReadFormat}/{@link ASignalWriteFormat}
+	using described streams for describes stream specific functionality
+	through {@link CDescrFlushObjListWriteFormat}/{@link CDescrObjListReadFormat}
+*/
+public class TestCDescrFlushObjListFormat_Descr extends ATestISignalFormat_Descr
+{
+		@Override protected Pair create(int max_name_length, int max_recursion_depth)
+		{
+			CObjListFormat media = new CObjListFormat();
+			
+			return new Pair(
+						new CDescrFlushObjListWriteFormat(
+									 8,//int names_registry_size,
+									 max_name_length,//int max_name_length,
+									 max_recursion_depth,//int max_events_recursion_depth,
+									 media //CObjListFormat media
+									 ),
+						new CDescrObjListReadFormat(
+									 8,//int names_registry_size,
+									 max_name_length,//int max_name_length,
+									 max_recursion_depth,//int max_events_recursion_depth
+									 media //CObjListFormat media
+									 ));
+		};
+};
